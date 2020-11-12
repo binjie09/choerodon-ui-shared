08f7321d19973a738fe7912fd84d866be134365f
HughHzWu <99571603@qq.com>
2020-11-12 14:06:07 +0800

    [ADD] ModalManager

--- src/modal-manager/index.tsx
+++ src/modal-manager/index.tsx
@@ -3,6 +3,17 @@ import { action, observable } from 'mobx';
 export interface IModalContainer {
 }
 
+export type MousePosition = { x: number; y: number };
+
+export type ModalManagerType = {
+  addInstance: (instance: IModalContainer) => void;
+  removeInstance: (instance: IModalContainer) => void;
+  getKey: () => string;
+  mousePositionEventBound: boolean;
+  mousePosition?: MousePosition;
+  containerInstances: IModalContainer[];
+}
+
 const KeyGen = (function* (id) {
   while (true) {
     yield `modal-${id}`;
@@ -10,16 +21,16 @@ const KeyGen = (function* (id) {
   }
 })(1);
 
-const containerInstances: IModalContainer[] = observable.array();
+const containerInstances: IModalContainer[] = observable.array<IModalContainer>();
 
-const removeInstance = action((instance: IModalContainer) => {
+const removeInstance: (instance: IModalContainer) => void = action((instance: IModalContainer) => {
   const index = containerInstances.indexOf(instance);
   if (index > -1) {
     containerInstances.splice(index, 1);
   }
 });
 
-const addInstance = action((instance: IModalContainer) => {
+const addInstance: (instance: IModalContainer) => void = action((instance: IModalContainer) => {
   removeInstance(instance);
   containerInstances.push(instance);
 });
@@ -28,7 +39,7 @@ function getKey(): string {
   return KeyGen.next().value;
 }
 
-const ModalManager = {
+const ModalManager: ModalManagerType = {
   addInstance,
   removeInstance,
   getKey,
